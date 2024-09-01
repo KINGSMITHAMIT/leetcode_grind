3dca4eae153dae5e8300a65b2958fd7f79238a06
KINGSMITHAMIT <142286016+KINGSMITHAMIT@users.noreply.github.com>
2024-09-01 22:08:56 +0530

    Time: 4 ms (69.98%) | Memory: 15.1 MB (77.96%) - LeetSync

--- 2058-concatenation-of-array/concatenation-of-array.cpp
+++ 2058-concatenation-of-array/concatenation-of-array.cpp
@@ -1,18 +1,13 @@
 class Solution {
 public:
     vector<int> getConcatenation(vector<int>& nums) {
-        int l = nums.size();
-        int n = 2 * l;
-        vector<int> ans(n); // Create a vector to hold the concatenated elements
-
-        // Copy the elements from the first array
-        for (int i = 0; i < l; i++)
+        vector<int> ans(2*nums.size());
+        for(int i =0; i< nums.size();i++){
             ans[i] = nums[i];
-
-        // Copy the elements from the second array
-        for (int i = 0; i < l; i++)
-            ans[i + l] = nums[i];
-
+        }
+        for(int i =0; i< nums.size();i++){
+            ans[i+nums.size()] = nums[i];
+        }
         return ans;
     }
 };
