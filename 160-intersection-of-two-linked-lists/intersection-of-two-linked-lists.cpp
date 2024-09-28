1bac7de6e0e47bb332247d5c27e335dfdc14495a
KINGSMITHAMIT <142286016+KINGSMITHAMIT@users.noreply.github.com>
2024-09-28 23:16:21 +0530

    Time: 34 ms (76.94%) | Memory: 17.4 MB (68.81%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
@@ -14,53 +14,32 @@ public:
         t1 = headA;
         t2 = headB;
         int d =0;
-        
-
-        if(l1==l2){
-            if(t1==t2) return t2;
-            else{ 
-            while (t2 != NULL){
-                if(t1->next == t2->next) return t2->next;
-                else{
-                    t1 = t1->next;
-                    t2 = t2->next;
-                }
-            }
-            }
-            return NULL;
-        }
-        else if(l2<l1) {
+        if(l2<l1) {
             d = l1-l2;
             for(int i =0; i < d; i++){
                 t1= t1->next;
             }
-            if(t1==t2) return t2;
-            while (t2 != NULL){
-                if(t1->next == t2->next) return t2->next;
-                else{
+            while(t1!=t2){
                     t1 = t1->next;
                     t2 = t2->next;
-                }
             }
-            return NULL;
+                return t1;
+
         }
     
-           else if(l1<l2) {
+           else {
             d = l2-l1;
             for(int i =0; i < d; i++){
                 t2= t2->next;
             }
-            if(t1==t2) return t2;
-            while (t1 != NULL){
-                if(t1->next == t2->next) return t1->next;
-                else{
+            while(t1!=t2){
                     t1 = t1->next;
                     t2 = t2->next;
-                }
+                
             }
-            return NULL;
+            return t1;
         }
-        return NULL;
+        
     }    
 
     
