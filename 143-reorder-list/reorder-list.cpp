f6ac80091c39f70e0c4784e856066c80c156afab
KINGSMITHAMIT <142286016+KINGSMITHAMIT@users.noreply.github.com>
2024-10-05 00:16:30 +0530

    Time: 33 ms (20.99%) | Memory: 22.6 MB (92.62%) - LeetSync

---
+++ 143-reorder-list/reorder-list.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head){
+        ListNode *prev = NULL, *curr = head, *next = head;
+        while(curr){
+            next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+    void reorderList(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast->next!= NULL && fast->next->next!= NULL){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        ListNode* b = reverseList(slow->next);
+        ListNode*a = head;
+        slow->next = NULL;
+        ListNode * c= new ListNode(1);
+        ListNode* tempC = c;
+        ListNode* tempA = a;
+        ListNode* tempB = b;
+        while(tempA && tempB){
+            tempC->next = tempA;
+            tempA = tempA->next;
+            tempC = tempC->next;
+
+            tempC->next = tempB;
+            tempB = tempB->next;
+            tempC = tempC->next;
+        }
+        tempC->next = tempA;
+        head =  c->next;
+    }
+};
