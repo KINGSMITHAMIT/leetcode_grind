7bc8d375b57643f34d9f23227ce1746bc5e7ac00
KINGSMITHAMIT <142286016+KINGSMITHAMIT@users.noreply.github.com>
2024-12-08 23:54:13 +0530

    Time: 40 ms (96.85%) | Memory: 126.2 MB (74.10%) - LeetSync

--- 2164-two-best-non-overlapping-events/two-best-non-overlapping-events.cpp
+++ 2164-two-best-non-overlapping-events/two-best-non-overlapping-events.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
     int maxTwoEvents(vector<vector<int>>& events) {
-        // Sort on end time
-        auto cmpEnd = [](const vector<int>& a, const vector<int>& b) {
-            return a[1] > b[1];
-        };
-        priority_queue<vector<int>, vector<vector<int>>, decltype(cmpEnd)> pq(cmpEnd);
-
         // Sort on start time
         sort(events.begin(), events.end(), [](const vector<int>& a, const vector<int>& b) {
             return a[0] < b[0];
         });
 
-        int prevMax = 0;
-        int res = 0;
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        int prevMax = 0, res = 0;
+
         for (const auto& event : events) {
-            // Find prevMax
-            while (!pq.empty() && pq.top()[1] < event[0]) {
-                prevMax = max(prevMax, pq.top()[2]);
+            // Maintain the max value of events that have ended before the current event starts
+            while (!pq.empty() && pq.top().first < event[0]) {
+                prevMax = max(prevMax, pq.top().second);
                 pq.pop();
             }
+
+            // Update the result with the maximum possible value of two non-overlapping events
             res = max(res, prevMax + event[2]);
-            pq.push(event);
+
+            // Add the current event to the priority queue
+            pq.emplace(event[1], event[2]);
         }
+
         return res;
     }
 };
