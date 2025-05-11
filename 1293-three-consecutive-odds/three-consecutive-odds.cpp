07bc558a6ab74085e34db1c0236ed9d095a10788
KINGSMITHAMIT <142286016+KINGSMITHAMIT@users.noreply.github.com>
2025-05-11 10:02:38 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (88.25%) - LeetSync

--- 1293-three-consecutive-odds/three-consecutive-odds.cpp
+++ 1293-three-consecutive-odds/three-consecutive-odds.cpp
@@ -4,6 +4,8 @@ public:
         if(arr.size()<3)return false;
         for(int i =0; i<arr.size()-2;i++){
             if((arr[i]&1)&&(arr[i+1]&1)&&(arr[i+2]&1)) return true;
+            if(!arr[i+1]&1)i++;
+            if(!arr[i+1]&1&&!arr[i+2]&1)i++;
         }
         return false;
     }
