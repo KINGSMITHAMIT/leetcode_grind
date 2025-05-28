6c91bfecd5490a101bdfa5fe41569c4e001de556
KINGSMITHAMIT <142286016+KINGSMITHAMIT@users.noreply.github.com>
2025-05-28 19:01:58 +0530

    Time: 27 ms (42.44%) | Memory: 88.2 MB (91.32%) - LeetSync

--- 525-contiguous-array/contiguous-array.cpp
+++ 525-contiguous-array/contiguous-array.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     int findMaxLength(vector<int>& v) {
-        map<int, int> mp;
+        unordered_map<int, int> ump;
         int ans= 0, sum=0;
         for(int i =0; i< v.size(); i++){
             if(v[i]== 0) sum+=-1;
             else sum+=1;
             if(sum ==0) ans= (i+1); // for the length of the possible no. of elemets of same no's of 1's and 0's
             else{
-                if(mp.count(sum)) ans= max(ans, i- mp[sum]);
-                else mp[sum] =i;
+                if(ump.count(sum)) ans= max(ans, i- ump[sum]);
+                else ump[sum] =i;
             }
         }
         return ans;
