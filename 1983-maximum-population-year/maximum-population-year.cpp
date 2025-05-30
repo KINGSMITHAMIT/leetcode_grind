e9a699c55a048aad6989e27d80cdda62ed7f4d66
KINGSMITHAMIT <142286016+KINGSMITHAMIT@users.noreply.github.com>
2025-05-30 21:12:21 +0530

    Time: 0 ms (100.00%) | Memory: 11.1 MB (74.43%) - LeetSync

--- 1983-maximum-population-year/maximum-population-year.cpp
+++ 1983-maximum-population-year/maximum-population-year.cpp
@@ -1,45 +1,47 @@
-// class Solution {
-// public:
-//     int maximumPopulation(vector<vector<int>>& logs) {
-//         map<int, int>mp;
-//         for(int i = 0; i<logs.size(); i++){
-//             mp[logs[i][0]]++;
-//             mp[logs[i][1]]--;
-//         }
-        
-//         int ans=INT_MIN;
-//         for(const auto &it: mp){
-//             if(it.second>ans)ans=it.first;
-//         }
-//         return ans;
-//     }
-// };
-
 class Solution {
 public:
     int maximumPopulation(vector<vector<int>>& logs) {
-        map<int, int> mp; // Difference array in map form
-
-        // Marking births and deaths
-        for(const auto& log : logs) {
-            mp[log[0]]++;  // Birth year increment
-            mp[log[1]]--;  // Death year decrement
+        map<int, int>mp;
+        for(int i = 0; i<logs.size(); i++){
+            mp[logs[i][0]]++;
+            mp[logs[i][1]]--;
         }
-
-        int maxPop = 0, currPop = 0, bestYear = INT_MAX;
         
-        // Compute running population count
-        for(const auto& year : mp) {
-            currPop += year.second;
-            
-            // If new max found, update
-            if (currPop > maxPop) {
-                maxPop = currPop;
-                bestYear = year.first;
+        int maxPop=INT_MIN,currPop=INT_MIN, ans; 
+        for(const auto &it: mp){
+            currPop+=it.second;
+            if(currPop>maxPop){
+                maxPop= currPop;
+                ans=it.first;
             }
         }
-
-        return bestYear;
+        return ans;
     }
 };
+
+// class Solution {
+// public:
+//     int maximumPopulation(vector<vector<int>>& logs) {
+//         map<int, int> mp;
+
+//            for(const auto& log : logs) {
+//             mp[log[0]]++;  
+//             mp[log[1]]--;  
+//         }
+
+//         int maxPop = 0, currPop = 0, bestYear = INT_MAX;
+        
+        
+//         for(const auto& year : mp) {
+//             currPop += year.second;
+            
+//             if (currPop > maxPop) {
+//                 maxPop = currPop;
+//                 bestYear = year.first;
+//             }
+//         }
+
+//         return bestYear;
+//     }
+// };
 
