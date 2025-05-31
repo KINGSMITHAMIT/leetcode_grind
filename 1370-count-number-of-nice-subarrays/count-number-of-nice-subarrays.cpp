b2c7fc44ceb3514d04fc6bf36cd579ebe6f13e91
KINGSMITHAMIT <142286016+KINGSMITHAMIT@users.noreply.github.com>
2025-06-01 02:19:50 +0530

    Time: 107 ms (8.00%) | Memory: 115 MB (6.67%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
@@ -3,7 +3,7 @@ public:
     int numberOfSubarrays(vector<int>& nums, int k) {
     using ll=long long;
     int ps=0, ans=0;
-    map<ll, ll>mp;
+    unordered_map<ll, ll>mp;
     mp[0]++;
     for(const auto &it : nums){
         if(it&1) ps+=1;
