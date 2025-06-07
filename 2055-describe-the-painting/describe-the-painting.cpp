75eaad82960734dab972f74dfaef2e3fbca7ed4a
KINGSMITHAMIT <142286016+KINGSMITHAMIT@users.noreply.github.com>
2025-06-07 17:55:45 +0530

    Time: 116 ms (39.33%) | Memory: 111.5 MB (42.00%) - LeetSync

--- 2055-describe-the-painting/describe-the-painting.cpp
+++ 2055-describe-the-painting/describe-the-painting.cpp
@@ -1,18 +1,23 @@
 class Solution {
 public:
-        vector<vector<long long>> splitPainting(vector<vector<int>>& A) {
-        map<int, long long> d;
-        for (auto& a : A) {
-            d[a[0]] += a[2];
-            d[a[1]] -= a[2];
+    using ll=long long;
+    static const int N=1e5+2; // max array can go upto 1e7;
+    ll v[N];
+    vector<vector<long long>> splitPainting(vector<vector<int>>& s) {
+        set<ll>st;
+        // vector<ll>v(20002,0);
+        for(int i=0; i<s.size(); i++){
+            st.insert(s[i][0]);
+            st.insert(s[i][1]);
+            v[s[i][0]]+=s[i][2];
+            v[s[i][1]]-=s[i][2];
         }
-        vector<vector<long long> > res;
-        int i = 0;
-        for (auto& it : d) {
-            if (d[i] > 0)
-                res.push_back({i, it.first, d[i]});
-            d[it.first] += d[i];
-            i = it.first;
+        for(ll i=1; i<(sizeof(v)/sizeof(v[0])); i++)v[i]+=v[i-1];  // sweep line
+        vector<ll>ans(st.begin(),st.end());
+        vector<vector<ll>>res;
+        for(int i =1; i<ans.size(); i++){
+            if(v[ans[i-1]]!=0)
+            res.push_back({ans[i-1],ans[i], v[ans[i-1]]});
         }
         return res;
     }
