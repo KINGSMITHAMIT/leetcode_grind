de6b1973d90e03ee87a7812705e0f23a3dde8880
KINGSMITHAMIT <142286016+KINGSMITHAMIT@users.noreply.github.com>
2025-06-16 19:56:13 +0530

    Time: 0 ms (100.00%) | Memory: 11.9 MB (71.76%) - LeetSync

--- 2144-maximum-difference-between-increasing-elements/maximum-difference-between-increasing-elements.cpp
+++ 2144-maximum-difference-between-increasing-elements/maximum-difference-between-increasing-elements.cpp
@@ -1,15 +1,27 @@
 class Solution {
 public:
     int maximumDifference(vector<int>& nums) {
-        int maxDiff= -1, diff= 0;
-       for(int i =0; i<nums.size(); i++){
-        for(int j=i+1; j<nums.size(); j++){
-            diff= nums[j]-nums[i];
-            maxDiff=max(maxDiff, diff);
+        //brute force
+    //     int maxDiff= -1, diff= 0;
+    //    for(int i =0; i<nums.size(); i++){
+    //     for(int j=i+1; j<nums.size(); j++){
+    //         diff= nums[j]-nums[i];
+    //         maxDiff=max(maxDiff, diff);
+    //     }
+    //    } 
+    //    if(maxDiff==0) return -1;
+    //    else return maxDiff;
+        //optimal approach - two pointer
+
+        int minEle = nums[0], maxDiff= 0, diff;
+        for(int i=1; i<nums.size(); i++){
+            if(nums[i]>minEle)  {
+                diff = nums[i]  - minEle;
+                maxDiff= max(maxDiff, diff);
+            }
+            else minEle= nums[i];   // if i never get any number bigger than the previous num then i won't be able to update the maxDiff 
         }
-       } 
-       if(maxDiff==0) return -1;
-       else
-       return maxDiff;
+        if(maxDiff == 0)    return -1;
+        else    return maxDiff;
     }
 };
