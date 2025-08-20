9b8e6afe795585f202151a2958d456c670ed059a
KINGSMITHAMIT <142286016+KINGSMITHAMIT@users.noreply.github.com>
2025-08-20 15:15:25 +0530

    Time: 3 ms (31.74%) | Memory: 16.1 MB (8.75%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,20 +1,23 @@
 class Solution {
 public:
-    vector<vector<int>>ans;
-    void sub(vector<int>&nums, vector<int>&subset, int i , int n ){
+    set<vector<int>>st;
+    void helper(vector<int> &nums, vector<int>subset, int i , int n){
         if(i>=n){
-            ans.push_back(subset);
+            st.insert(subset);
             return;
         }
         subset.push_back(nums[i]);
-        sub(nums, subset, i+1, n);
-        subset.pop_back();// backtracking to prevent the passage of same subset in the empty line
-        sub(nums, subset, i+1, n);
+        helper(nums, subset, i+1,n);
+        subset.pop_back(); // backtrack logic
+        //new subset
+        helper(nums, subset, i+1,n);
     }
     vector<vector<int>> subsets(vector<int>& nums) {
         int n = nums.size();
         vector<int> subset;
-        sub(nums, subset, 0, n);
-        return ans;
+        helper(nums, subset, 0, n);
+        vector<vector<int>> ans;
+        for( auto &it : st) ans.push_back(it);
+        return ans; 
     }
 };
