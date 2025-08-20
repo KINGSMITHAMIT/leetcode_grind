15d1728ccd49f65f9a2bc6d76f98ed85abc546ab
KINGSMITHAMIT <142286016+KINGSMITHAMIT@users.noreply.github.com>
2025-08-20 14:24:36 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (42.29%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,33 +1,20 @@
 class Solution {
 public:
-
-
-void helper(vector<int>& nums, vector<int> ans, vector<vector<int>>& finalAns, int idx){
-
-if(idx==nums.size()){
-
-finalAns.push_back(ans);
-
-return;
-
-}
-
-helper(nums, ans, finalAns, idx+1);
-
-ans.push_back(nums[idx]);
-
-helper(nums,ans, finalAns, idx+1);
-
-}
-
-vector<vector<int>> subsets (vector<int>& nums) {
-
-vector<int> ans;
-
-vector<vector<int>> finalAns;
-
-helper(nums,ans, finalAns, 0);
-
-return finalAns;
-}
+    vector<vector<int>>ans;
+    void sub(vector<int>&nums, vector<int>&subset, int i , int n ){
+        if(i>=n){
+            ans.push_back(subset);
+            return;
+        }
+        subset.push_back(nums[i]);
+        sub(nums, subset, i+1, n);
+        subset.pop_back();// backtracking to prevent the passage of same subset in the empty line
+        sub(nums, subset, i+1, n);
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> subset;
+        sub(nums, subset, 0, n);
+        return ans;
+    }
 };
