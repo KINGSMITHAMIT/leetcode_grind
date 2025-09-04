7f2ace12d2c91260185f7ce9873c49c77e4dc7a2
KINGSMITHAMIT <142286016+KINGSMITHAMIT@users.noreply.github.com>
2025-09-04 13:58:34 +0530

    Time: 5 ms (57.69%) | Memory: 32.5 MB (92.13%) - LeetSync

--- 3278-find-the-number-of-ways-to-place-people-i/find-the-number-of-ways-to-place-people-i.cpp
+++ 3278-find-the-number-of-ways-to-place-people-i/find-the-number-of-ways-to-place-people-i.cpp
@@ -1,43 +1,39 @@
 class Solution {
 public:
-    int numberOfPairs(vector<vector<int>>& points) {
-        int ans = 0;
-        int n = points.size();
-
-        for (int i = 0; i < n; i++) {
-            auto& pointA = points[i];
-            for (int j = 0; j < n; j++) {
-                vector<int> pointB = points[j];
-                if (i == j ||
-                    !(pointA[0] <= pointB[0] && pointA[1] >= pointB[1])) {
-                    continue;
-                }
-                if (n == 2) {
-                    ans++;
-                    continue;
-                }
-
-                bool illegal = false;
-                for (int k = 0; k < n; k++) {
-                    if (k == i || k == j) {
-                        continue;
-                    }
-
-                    auto& pointTmp = points[k];
-                    bool isXContained =
-                        pointTmp[0] >= pointA[0] && pointTmp[0] <= pointB[0];
-                    bool isYContained =
-                        pointTmp[1] <= pointA[1] && pointTmp[1] >= pointB[1];
-                    if (isXContained && isYContained) {
-                        illegal = true;
+//the solution to the code has been written to find the lower right side point
+    bool customSort(vector<int>a, vector<int>b){
+        if(a[0]==b[0]) return a[1]>b[1];
+        return a[0]<b[0]; 
+    }
+    int numberOfPairs(vector<vector<int>>& v) {
+        int n = v.size();
+        int count=0;
+        // sort(vector<vector<int>>points.begin(), vector<vector<int>>point.end(), customSort);
+        for(int i =0; i<n ; i++){
+            //the upper point
+            int x1= v[i][0];
+            int y1= v[i][1];
+            for(int j =0; j<n; j++ ){
+                if(i == j )continue;
+                //the lower point
+                int x2 = v[j][0];
+                int y2 = v[j][1];
+                // this point is lower obviously but still do we not have to check wether there exist a point in between them
+                if(x1<=x2&&y1>=y2){
+                    bool isPointInside= false;
+                    for(int k = 0; k<n; k++){
+                    if(k == i || j == k) continue;
+                    int x3 = v[k][0];
+                    int y3 = v[k][1];
+                    if((x3>=x1&&x3<=x2)&&(y3>=y2&&y3<=y1)){
+                        isPointInside= true;
                         break;
                     }
-                }
-                if (!illegal) {
-                    ans++;
+                    }
+                    if(!isPointInside) count++;
                 }
             }
         }
-        return ans;
+        return count;
     }
 };
