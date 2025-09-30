d87693572d814c01f1a79723e8b3442f6bacc33c
KINGSMITHAMIT <142286016+KINGSMITHAMIT@users.noreply.github.com>
2025-10-01 02:06:41 +0530

    Time: 375 ms (15.80%) | Memory: 383.8 MB (10.86%) - LeetSync

--- 2324-find-triangular-sum-of-an-array/find-triangular-sum-of-an-array.cpp
+++ 2324-find-triangular-sum-of-an-array/find-triangular-sum-of-an-array.cpp
@@ -5,7 +5,7 @@ public:
         while(v.size()!=1){
             vector<int>ans;
             for(int i =0 ; i< v.size()-1; i++) ans.push_back((v[i]+v[i+1])%10);
-            v = ans;
+            v = move(ans); // this prevents us from making a copy of ans vector and then transfering it in to my vector v the stl of c++ allows us to do in through move which directly empt
         }
         return v[0];
     }
