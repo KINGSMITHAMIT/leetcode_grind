e13b53b34447ecdb855b547948330df3f1cd24c0
KINGSMITHAMIT <142286016+KINGSMITHAMIT@users.noreply.github.com>
2025-10-06 00:03:12 +0530

    Time: 3 ms (71.07%) | Memory: 13.3 MB (74.68%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<string>ans;
+    void help(int i ,int n, int openBrac, string &st){
+        if(i>=(n*2)) {
+           if(openBrac == 0) ans.push_back(st);
+            return;
+        }
+        // i can go with an '(' at the start
+        st+='(';
+        help(i+1, n, openBrac+1, st);
+        st.pop_back(); // for backtrack
+        // i can push an ')' only if there is a open brac 
+        if(openBrac >0 ) {
+            st+=')';
+            help(i+1,n, openBrac-1, st);
+            st.pop_back();
+        }
+        
+    }
+    vector<string> generateParenthesis(int n) {
+        string st="";
+        help(0,n,0,st);
+        return ans;   
+    }
+};
