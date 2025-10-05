eef9058f9d6629e46fb83df6f8212ecc754220c6
KINGSMITHAMIT <142286016+KINGSMITHAMIT@users.noreply.github.com>
2025-10-06 00:35:52 +0530

    Time: 3 ms (71.07%) | Memory: 15.6 MB (49.33%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -1,14 +1,21 @@
 class Solution {
 public:
-    vector<string>ans;
-    void help(string st, int open ,int close, int n ){
-        // if all my open brac == close brac then i can push this into my ans
-        if(open==n&&close==n&& open==close ){ans.push_back(st);return;}
-        if(open<n) help(st+'(', open+1, close, n);
-        if(open>close) help(st+')', open, close+1,n);
-    }
     vector<string> generateParenthesis(int n) {
-        help("(",1,0,n);
+        vector<string> ans;
+
+        appendOne("(", 1, 0, n, ans);
         return ans;
+    }
+
+    void appendOne(string s, int open, int close, int n,  vector<string>& ans) {
+        if(open < n) {
+            appendOne(s+"(", open+1, close, n, ans);
+        }
+        if((open > close) && (close < n)) {
+            appendOne(s+")", open, close+1, n, ans);
+        }
+        if(open == n && close == n) {
+            ans.push_back(s);
+        }
     }
 };
