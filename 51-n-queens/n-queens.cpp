0a17a4db5eb30a48cadec131a7cf948cd0a215c7
KINGSMITHAMIT <142286016+KINGSMITHAMIT@users.noreply.github.com>
2025-10-05 16:15:07 +0530

    Time: 3 ms (65.91%) | Memory: 10.4 MB (39.00%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -1,70 +1,39 @@
 class Solution {
 public:
     vector<vector<string>>ans;
-    bool check(vector<string>&board, int r, int c, int n){
-        for(int i = 0; i<=r ; i++)  if(board[i][c] == 'Q') return 0;
-        for(int  i = r-1, j = c-1; i>=0&& j>=0; i--, j--) if(board[i][j]=='Q') return 0;
-        for(int i = r-1, j =c+1;i>=0&& j<n; i--, j++  ) if(board[i][j]=='Q') return 0;
+    bool check(int r , int c,int n,vector<string>&board){
+        //check if there is any Q +nt in the row 
+        for(int i =0 ; i<r; i++) if(board[i][c]=='Q') return false;
+        //chk if there is any Q +nt it the left diagonal and the right diagonal
+        // Left diagonal
+for(int i = r-1, j = c-1; i >= 0 && j >= 0; i--, j--) {
+    if(board[i][j] == 'Q') return false;
+}
+
+// Right diagonal
+for(int i = r-1, j = c+1; i >= 0 && j < n; i--, j++) {
+    if(board[i][j] == 'Q') return false;
+}
         return 1;
     }
-    void help(vector<string>&board, int r, int n){
-        if(r==n) { 
-            ans.push_back(board);   
+    void help( int r, int n, vector<string>&board ){
+         if(r>=n){
+            ans.push_back(board);
             return;
-            }
-        for(int i = 0 ; i< n; i++){
-            if(check(board, r, i,  n )){
-                board[r][i]='Q';
-                help(board, r+1, n);
-                board[r][i]='.';
-            }
-        }
+         }
+         for(int i =0; i<n; i++){
+            if(check(r,i,n, board)==false) continue;
+            board[r][i]='Q';
+            help(r+1,n, board);
+            board[r][i]='.'; 
+         }
+         
     }
     vector<vector<string>> solveNQueens(int n) {
+        // let's create a puzzle
         string temp(n,'.');
         vector<string>board(n,temp);
-        help(board, 0, n);
+        help(0,n,board);
         return ans;
     }
 };
-
-// class Solution {
-// public:
-//     vector<vector<string>> ans;
-
-//     bool check(vector<string>& board, int r, int c, int n) {
-//         // check column
-//         for (int i = 0; i < r; i++)
-//             if (board[i][c] == 'Q') return false;
-
-//         // check upper-left diagonal
-//         for (int i = r - 1, j = c - 1; i >= 0 && j >= 0; i--, j--)
-//             if (board[i][j] == 'Q') return false;
-
-//         // check upper-right diagonal
-//         for (int i = r - 1, j = c + 1; i >= 0 && j < n; i--, j++)
-//             if (board[i][j] == 'Q') return false;
-
-//         return true;
-//     }
-
-//     void helper(vector<string>& board, int r, int n) {
-//         if (r == n) {
-//             ans.push_back(board); // push the whole board (vector<string>)
-//             return;
-//         }
-//         for (int i = 0; i < n; i++) {
-//             if (check(board, r, i, n)) {
-//                 board[r][i] = 'Q';
-//                 helper(board, r + 1, n);
-//                 board[r][i] = '.'; // backtrack
-//             }
-//         }
-//     }
-
-//     vector<vector<string>> solveNQueens(int n) {
-//         vector<string> board(n, string(n, '.')); // n rows, each row is "...."
-//         helper(board, 0, n);
-//         return ans;
-//     }
-// };
