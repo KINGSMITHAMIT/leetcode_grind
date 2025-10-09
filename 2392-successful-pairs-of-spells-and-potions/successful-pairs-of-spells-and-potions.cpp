f454fafc320dd54d3ead1a69067d38be52fdbff4
KINGSMITHAMIT <142286016+KINGSMITHAMIT@users.noreply.github.com>
2025-10-09 14:35:57 +0530

    Time: 44 ms (53.85%) | Memory: 128.5 MB (8.60%) - LeetSync

--- 2392-successful-pairs-of-spells-and-potions/successful-pairs-of-spells-and-potions.cpp
+++ 2392-successful-pairs-of-spells-and-potions/successful-pairs-of-spells-and-potions.cpp
@@ -1,27 +1,20 @@
 class Solution {
 public:
-    vector<int>ans;
-    void bs(vector<int>&potions, int spell, long long success ){
-        int n = potions.size();
-        long long minpotion =ceil((1.0*success)/spell);
-        if(minpotion>potions[n-1]){
-            ans.push_back(0);
-            return;
-        }
-        int low = 0;
-        int hi = n-1;
-        while(low < hi){
-            int mid = low+(hi-low)/2;
-            if(potions[mid]<minpotion) low=mid+1;
-            else hi=mid;
-        }
-        ans.push_back(n-low);
-    }
     vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
         int m = spells.size();
+        int n = potions.size();
+        vector<int>ans;
         sort(potions.begin(),potions.end());
-        for(int i = 0; i<m; i++){
-            bs(potions, spells[i], success);
+        int max_potion=potions[n-1];
+        // let's try interating over the spells array and find the lowerbound of the minpotin for each spell
+        for(int i = 0; i< m; i++){
+            long long min_potion= ceil((1.0*success)/spells[i]);
+            if(min_potion>max_potion){
+                ans.push_back(0);
+                continue;
+            }
+            int pos=lower_bound(potions.begin(), potions.end(),min_potion)-potions.begin();
+            ans.push_back(n-pos);
         }
         return ans;
     }
