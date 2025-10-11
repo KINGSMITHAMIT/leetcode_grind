5854633627c827cd01ec7f696dff45c6ee5a7121
KINGSMITHAMIT <142286016+KINGSMITHAMIT@users.noreply.github.com>
2025-10-11 21:48:15 +0530

    Time: 4 ms (31.32%) | Memory: 14 MB (43.08%) - LeetSync

--- 241-different-ways-to-add-parentheses/different-ways-to-add-parentheses.cpp
+++ 241-different-ways-to-add-parentheses/different-ways-to-add-parentheses.cpp
@@ -1,61 +1,26 @@
 class Solution {
 public:
     vector<int> diffWaysToCompute(string expression) {
-        vector<int> results;
-
-        // Base case: if the string is empty, return an empty list
-        if (expression.length() == 0) return results;
-
-        // Base case: if the string is a single character, treat it as a number
-        // and return it
-        if (expression.length() == 1) {
-            results.push_back(stoi(expression));
-            return results;
+        vector<int>result;
+        if(expression.size()<=2){
+            result.push_back(stoi(expression));
+            return result;
         }
-
-        // If the string has only two characters and the first character is a
-        // digit, parse it as a number
-        if (expression.length() == 2 && isdigit(expression[0])) {
-            results.push_back(stoi(expression));
-            return results;
-        }
-
-        // Recursive case: iterate through each character
-        for (int i = 0; i < expression.length(); i++) {
-            char currentChar = expression[i];
-
-            // Skip if the current character is a digit
-            if (isdigit(currentChar)) continue;
-
-            // Split the expression into left and right parts
-            vector<int> leftResults =
-                diffWaysToCompute(expression.substr(0, i));
-            vector<int> rightResults =
-                diffWaysToCompute(expression.substr(i + 1));
-
-            // Combine results from left and right parts
-            for (int leftValue : leftResults) {
-                for (int rightValue : rightResults) {
-                    int computedResult = 0;
-
-                    // Perform the operation based on the current character
-                    switch (currentChar) {
-                        case '+':
-                            computedResult = leftValue + rightValue;
-                            break;
-                        case '-':
-                            computedResult = leftValue - rightValue;
-                            break;
-                        case '*':
-                            computedResult = leftValue * rightValue;
-                            break;
+        int n = expression.size();
+        for(int i =0; i<n; i++){
+            if(isdigit(expression[i]))continue;
+            vector<int>left= diffWaysToCompute(expression.substr(0,i));
+            vector<int>right= diffWaysToCompute(expression.substr(i+1));
+                int val=0;
+                for(auto &lv: left){
+                    for(auto &rv:right){
+                        if(expression[i]=='+') val = lv+rv;
+                        else if(expression[i]=='-') val = lv-rv;
+                        else val = lv*rv;
+                        result.push_back(val);
                     }
-
-                    results.push_back(computedResult);
                 }
-            }
         }
-
-        return results;
+        return result;
     }
 };
