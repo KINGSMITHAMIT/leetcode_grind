1e774641624e5003a3bac5f888dcfe0d5530405e
KINGSMITHAMIT <142286016+KINGSMITHAMIT@users.noreply.github.com>
2025-10-18 19:34:28 +0530

    Time: 580 ms (5.01%) | Memory: 17.3 MB (88.29%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
@@ -1,14 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
 class Solution {
 public:
+    // let's write the bool case for checking wether or not the values are present or not 
+    // the basic idea is simple to return wether or not the value is present or not 
     bool exists(TreeNode* root, TreeNode* target){
-        if(root==NULL) return false;
-        if(root==target) return true;
-        return exists(root->left, target)||exists(root->right, target);
+        if(root==NULL) return 0;
+        if(root==target) return 1;
+        return exists(root->left, target) || exists(root->right,target);
     }
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if(exists(root->left, p)&& exists(root->left, q)) return lowestCommonAncestor(root->left, p, q);
-        else if(exists(root->right, p)&& exists(root->right, q)) return lowestCommonAncestor(root->right, p, q);
-        return root;
-
+        if(root==p || root== q) return root; 
+        if(exists(root->right, p)&&exists(root->left, q)) return root;
+        if(exists(root->left, p)&&exists(root->right, q)) return root;
+        else if(exists(root->left, p)&&exists(root->left, q)) return lowestCommonAncestor(root->left, p , q);
+        else return lowestCommonAncestor(root->right, p, q);
     }
 };
