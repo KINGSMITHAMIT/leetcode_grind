205f0e0c060a7b009169885ee91ac12adc14f417
KINGSMITHAMIT <142286016+KINGSMITHAMIT@users.noreply.github.com>
2025-10-25 21:31:54 +0530

    Time: 0 ms (100.00%) | Memory: 11 MB (42.70%) - LeetSync

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.cpp
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.cpp
@@ -9,20 +9,25 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
+class Solution { // left right root 
 public:
-    void solve(TreeNode* root, vector<int>& ans){
-        if(root==NULL) {
-            return;
-        }
-        solve(root->left, ans);
-        solve(root->right, ans);
-        ans.push_back(root->val);
+    // left right root ->in order to do that we will try to basically have it in my ans vector we will try to stare the elements in my ans vector in a reverse manner that is
+// we will try to push the root then left then right by that means when we try to reverse the ans array we will have our postorder array
+void postOrderTraversalIterative(TreeNode*root, vector<int>&ans){  
+    stack<TreeNode*>st;
+    if(root!=NULL)st.push(root); 
+    while(!st.empty()){
+            TreeNode* node = st.top(); 
+            st.pop();   
+            ans.push_back(node->val);   
+            if(node->left!=NULL)st.push(node->left);
+            if(node->right!=NULL)st.push(node->right);
     }
+    reverse(ans.begin(), ans.end());
+}
     vector<int> postorderTraversal(TreeNode* root) {
-        // let's create a vector of ans to store the values in ans  
         vector<int>ans;
-        solve(root, ans);
+        postOrderTraversalIterative(root, ans);
         return ans;
     }
 };
