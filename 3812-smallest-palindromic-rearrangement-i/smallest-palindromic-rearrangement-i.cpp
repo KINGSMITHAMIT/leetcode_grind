d40e53924ebc3e6cf4b6719ac808423be00ee466
AMIT <142286016+KINGSMITHAMIT@users.noreply.github.com>
2026-07-29 08:11:54 +0530

    Time: 59 ms (46.42%) | Memory: 69.1 MB (70.72%) - LeetSync

--- 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.cpp
+++ 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
     string smallestPalindrome(string s) {
-        int len = s.length();
-        int partition = len / 2;
-
-        sort(s.begin(), s.begin() + partition);
-
-        for (int i = 0; i < partition; ++i) {
-            s[len - 1 - i] = s[i];
-        }
-
-        return s;
+        int n = s.size();
+        int len = n/2;
+        string str = s.substr(0, len);
+        sort(str.begin(), str.end());
+        string rev_str =str; 
+        reverse(rev_str.begin(),rev_str.end());
+        if(n&1) return str+s[len]+rev_str;
+        else return str+rev_str;
     }
 };
