710bfeccfa0632cba864496b0468d580e412465c
AMIT <142286016+KINGSMITHAMIT@users.noreply.github.com>
2026-08-12 13:53:59 +0530

    Time: 210 ms (5.34%) | Memory: 150.7 MB (16.74%) - LeetSync

--- 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.cpp
+++ 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.cpp
@@ -1,24 +1,29 @@
 class Solution {
 public:
+    // WE NEED TO iterate over the array 
+    // we can create a measure of the max count of nums that can be accumulated 
+    // in this case the criteria is the no. of char does not exceed a limit 
+    // we will have to move the window as soon as the freq rises above k 
+    // but at that time should we increase the i pointer 
+    // by increasing the i counter we will decrease the freq count of a char 
+    // that need not had to be the char which has the freq cout above k
+    // so should we stat pointig the i pointer to the j location and then again start counting from the start will we get a usual big value 
+    // this probably seems right to me 
+    // let's do  this 
     int maxSubarrayLength(vector<int>& nums, int k) {
-        map<int, int> mp;
-
-        int i = 0, j = 0;
-        int n = nums.size();
-        int ans = 0;
-
-        while (j < n) {
+         map<long long, int>mp;
+        //  for(auto &x : nums) mp[x]++;
+        int i =0, j =0, n = nums.size(),  ans = 0;
+        
+        while(j<n){
             mp[nums[j]]++;
-
-            while (mp[nums[j]] > k) {
+            while(mp[nums[j]]>k) {
                 mp[nums[i]]--;
-                i++;
+                i++; 
             }
-
-            ans = max(ans, j - i + 1);
+            ans = max (ans, j-i+1);
             j++;
         }
-
         return ans;
     }
 };
