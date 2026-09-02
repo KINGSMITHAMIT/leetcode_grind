3c22cf75c9662b4df98a989de4fa71a1f31ee317
AMIT <142286016+KINGSMITHAMIT@users.noreply.github.com>
2026-09-02 21:02:28 +0530

    Time: 0 ms (100.00%) | Memory: 30.3 MB (36.43%) - LeetSync

---
+++ 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.cpp
@@ -0,0 +1,17 @@
+/*
+n distinct integers  num1
+n  all odd or all even  num2
+
+nums2[i] = nums1[i]
+nums2[i] = nums1[i] - nums1[j], for an index j != i
+
+
+*/
+
+
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        return 1;
+    }
+};
